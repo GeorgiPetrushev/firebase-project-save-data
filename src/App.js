376d0b22b5f7e8437acbd9950ad4b6526c079774
Georgi <figoto660@gmail.com>
2022-10-06 22:19:20 -0400

    save

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { useState, useEffect } from "react";
 import { db } from "./firebase/firebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 import { async } from "@firebase/util";
 
 function App() {
-  const[newName,setNewName] = useState("");
-  const[newAge,setNewAge] = useState(0)
+  const [newName, setNewName] = useState("");
+  const [newAge, setNewAge] = useState(0);
   const [users, setUsers] = useState([]);
   const usersCollectionRef = collection(db, "users");
 
-  const createUser = async() =>{
-
-  } 
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, { name: newName, age: newAge });
+    await setNewAge(0);
+    await setNewName("");
+  };
 
   useEffect(() => {
     const getUsers = async () => {
@@ -24,9 +26,18 @@ function App() {
 
   return (
     <div className="App">
-      <input placeholder="Name" onChange={(e)=> setNewName(e.target.value)} value={newName}></input>
-      <input placeholder="Age" type="number" onChange={(e)=> setNewAge(e.target.value)} value={newAge}></input>
-      <button>Add User</button>
+      <input
+        placeholder="Name"
+        onChange={(e) => setNewName(e.target.value)}
+        value={newName}
+      ></input>
+      <input
+        placeholder="Age"
+        type="number"
+        onChange={(e) => setNewAge(e.target.value)}
+        value={newAge}
+      ></input>
+      <button onClick={createUser}>Add User</button>
       {users.map((user) => {
         return (
           <div key={user.id}>
