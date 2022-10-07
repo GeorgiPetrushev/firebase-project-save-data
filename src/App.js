4dc6a7cf4c0750cbc7ea26985db5ec0ca4466cda
Georgi <figoto660@gmail.com>
2022-10-06 22:00:58 -0400

    connect firebase db to the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { useState, useEffect } from "react";
+import { db } from "./firebase/firebaseConfig";
+import { collection, doc, getDocs } from "firebase/firestore";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Test</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <div> Name: {user.name}</div> <div>Age:{user.age}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
